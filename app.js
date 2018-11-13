0d118aeb3203df258750d8fefe27c2eba9ecad25
kaza980 <anickeewa.iula@mail.ru>
2018-11-13 12:42:37 +0400

    Change comment text

--- app.js
+++ app.js
@@ -5,103 +5,36 @@
 // }
 
 /*
-
-<div>
-    <div className="logo-block">
-        <img id="logo" src="img/w-mercury-development.svg"/>
-    </div>
-    <div className="main-block">
-        <form id="login-form" name="loginForm" onsubmit="sendLoginForm(); disableForm(); return false">
-
-            <div className="main-block__title">
-                <p>Log In</p>
-            </div>
-
-            <div className="center main-block__input">
-                <input id="email" className="main-block__input-field " name="email" type="email" placeholder="E-mail"
-                       required
-                       title="Enter Email"/>
-            </div>
-            <div className="center main-block__input">
-                <input id="password" className="main-block__input-field" name="password" type="password"
-                       placeholder="Password"
-                       required title="Enter Password"/>
-            </div>
-            <div className="center" id="error-block">
-                <p id="error-message"></p>
-            </div>
-            <div className="center main-block__button">
-                <input type="submit" value="Login" className="main-block__button-field"/>
-            </div>
-        </form>
-    </div>
+<div className="logo-block">
+   <img id="logo" src={params.src}/>
 </div>
-*/
 
-/*function RenderFunction(props) {
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "div",
-            {className: "logo-block"},
-            React.createElement("img", {id: "logo", src: "img/w-mercury-development.svg"})
-        ),
-        React.createElement("div", {className: "main-block"},
-            React.createElement(
-                "form",
-                {id: "login-form", name: "loginForm"},
-                React.createElement(
-                    "div",
-                    {className: "main-block__title"},
-                    React.createElement(
-                        "p",
-                        null,
-                        "Log In"
-                    )
-                ),
-                React.createElement(
-                    "div",
-                    {className: "center main-block__input"},
-                    React.createElement("input", {
-                        id: "email",
-                        className: "main-block__input-field ",
-                        name: "email",
-                        type: "email",
-                        placeholder: "E-mail",
-                        required: true,
-                        title: "Enter Email"
-                    })
-                ),
-                React.createElement(
-                    "div",
-                    {className: "center main-block__input"},
-                    React.createElement("input", {
-                        id: "password",
-                        className: "main-block__input-field",
-                        name: "password",
-                        type: "password",
-                        placeholder: "Password",
-                        required: true,
-                        title: "Enter Password"
-                    })
-                ),
-                React.createElement(
-                    "div",
-                    {className: "center", id: "error-block"},
-                    React.createElement("p", {id: "error-message"})
-                ),
-                React.createElement(
-                    "div",
-                    {className: "center main-block__button"},
-                    React.createElement("input", {
-                        type: "submit",
-                        value: "Login",
-                        className: "main-block__button-field"
-                    })
-                )))
-    );
-}*/
+<div className="main-block">
+    <form id="login-form" name="loginForm" onsubmit="sendLoginForm(); disableForm(); return false">
+
+        <div className="main-block__title">
+            <p>Log In</p>
+        </div>
+
+        <div className="center main-block__input">
+            <input id="email" className="main-block__input-field " name="email" type="email" placeholder="E-mail"
+                   required
+                   title="Enter Email"/>
+        </div>
+        <div className="center main-block__input">
+            <input id="password" className="main-block__input-field" name="password" type="password"
+                   placeholder="Password"
+                   required title="Enter Password"/>
+        </div>
+        <div className="center" id="error-block">
+            <p id="error-message"></p>
+        </div>
+        <div className="center main-block__button">
+            <input type="submit" value="Login" className="main-block__button-field"/>
+        </div>
+    </form>
+</div>
+    */
 
 function RenderLogoFunction(params) {
     return React.createElement(
